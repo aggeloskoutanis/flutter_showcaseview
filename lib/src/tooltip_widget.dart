76d3eab13413fbd9aeb3cf77b1b3b28a46de3fa5
Aggelos Koutanis <aggelos@vendora.gr>
2022-10-10 14:15:03 +0300

    testing

--- lib/src/tooltip_widget.dart
+++ lib/src/tooltip_widget.dart
@@ -363,7 +363,7 @@ class _ToolTipWidgetState extends State<ToolTipWidget>
         children: <Widget>[
           Positioned(
             left: _getSpace() - 10,
-            top: contentY - 30,
+            top: contentY,
             child: FractionalTranslation(
               translation: Offset(0.0, contentFractionalOffset as double),
               child: SlideTransition(
